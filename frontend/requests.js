cf526303dff489f3d2c43e3d2a997efbadbfd825
Deni Lark <denlarsites@gmail.com>
2023-03-05 20:57:37 +0500

    frontend: passing tests works

--- frontend/requests.js
+++ frontend/requests.js
@@ -25,59 +25,76 @@ function submitTest(test) {
     .then((json) => console.log(json));
 }
 
-// получить тест
+
+// получить тест, и пройти его
 function getTest(idTest, modalWindow) {
   let currentQuestion = 0
+  let errorsCount = 0
+  let correctCount = 0
+
   const blockQuestionText = modalWindow.querySelector(".test-question")
   const blockAnswers = modalWindow.querySelector(".test-answers")
 
+  const blockInfo = modalWindow.querySelector(".block-info")
   const blockCurentNumber = modalWindow.querySelector(".number-current")
   const blockTotalQuestions = modalWindow.querySelector(".number-total")
   const blockErrors = modalWindow.querySelector(".number-error")
 
-  let errorsCount = 0
-  let correctCount = 0
-
-  blockErrors.innerHTML = errorsCount
+  blockInfo.classList.remove("block-info_hidden")
 
   fetch("http://localhost:8080/tests/" + idTest)
     .then(res => res.json())
     .then(test => {
       showQuestion(test.questions, currentQuestion)
 
-      blockAnswers.addEventListener("click", e => {
+      blockAnswers.addEventListener("click", blockAnswersHandler)
+
+      function blockAnswersHandler(e) {
         if (e.target.classList.contains("btn-answer")) {
-          currentQuestion++
-          showQuestion(test.questions, currentQuestion)
+          const idAnswer = e.target.dataset.answer
+
+          fetch("http://localhost:8080/answer/" + idAnswer)
+            .then(res => res.json())
+            .then(isCorrect => {
+              isCorrect ? correctCount++ : errorsCount++
+              currentQuestion++
+              if (currentQuestion + 1 > test.questions.length) {
+                const finishTestMsg =
+                  `
+                  <div class="test-finished">
+                    <h3>Поздравляем!</h3>
+                    <h4>Вы прошли тест</h4>
+                    <p>Правильных ответов: 
+                    <span class="number-correct">${correctCount}</span>
+                    </p>
+                    <p>Неправильных ответов: 
+                    <span class="number-error-container">${errorsCount}</span>
+                    </p>
+                  </div>
+                  `
+                blockQuestionText.innerHTML = finishTestMsg
+                blockAnswers.innerHTML = ""
+                blockInfo.classList.add("block-info_hidden")
+                blockAnswers.removeEventListener("click", blockAnswersHandler)
+              } else showQuestion(test.questions, currentQuestion)
+            })
         }
-      })
+      }
     })
 
   function showQuestion(questions, currentQuestion) {
-    if (currentQuestion + 1 >= questions.length) {
-      const finishTestMsg =
-        `
-        <div class="test-finished">
-          <h3>Поздравляем!</h3>
-          <h4>Вы прошли тест</h4>
-          <p>Правильных ответов: 
-           <span class="number-correct">${errorsCount}</span>
-          </p>
-          <p>Неправильных ответов: 
-           <span class="number-error-container">${correctCount}</span>
-          </p>
-        </div>
-        `
-      blockQuestionText.innerHTML = finishTestMsg
-    }
+    console.log(currentQuestion, questions.length)
 
     blockCurentNumber.innerHTML = currentQuestion + 1
     blockTotalQuestions.innerHTML = questions.length
+    blockErrors.innerHTML = errorsCount
 
     blockQuestionText.innerHTML = questions[currentQuestion].textQuestion
 
     let htmlButtons = questions[currentQuestion].answers.reduce((acc, a) => {
-      return (acc += `<button class="btn-answer">${a.textAnswer}</button>`)
+      return (acc += `<button data-answer="${a.answerId}" 
+                              class="btn-answer">${a.textAnswer}
+                      </button>`)
     }, "")
     blockAnswers.innerHTML = htmlButtons
   }
